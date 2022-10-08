46dfa7186e8a371689c35b7f36a5c6f2937f0107
Seb-SOULIER <soulier.sebastien.ss@gmail.com>
2022-10-08 17:58:43 +0200

    test6

--- src/Controller/Test1Controller.php
+++ src/Controller/Test1Controller.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class Test1Controller extends AbstractController
 {
     /**
-     * @Route("/test1", name="app_test1")
+     * @Route("/test1", name="app_test1", host="test.cod4y.fr")
      */
     public function index(): Response
     {
